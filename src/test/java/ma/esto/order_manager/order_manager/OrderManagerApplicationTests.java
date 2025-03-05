654855599cbffc130984a6ca5d84ca8c62d6db87
QALQOL Reda <redaqal@gmail.com>
2025-03-05 15:43:33 +0000

    initialising the project

---
+++ src/test/java/ma/esto/order_manager/order_manager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.esto.order_manager.order_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
